81b4c282b8ba00a0e265316ce2078a3abc13c0c1
Xwilarg <xwilarg@yahoo.fr>
2018-11-05 23:47:22 +0100

    Add empty bot

---
+++ Choukai/Program.cs
@@ -0,0 +1,96 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Choukai
+{
+    class Program
+    {
+        public static void Main(string[] args)
+              => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public readonly DiscordSocketClient client;
+        private readonly IServiceCollection map = new ServiceCollection();
+        private readonly CommandService commands = new CommandService();
+
+        private Program()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            client.Log += Log;
+            commands.Log += LogError;
+        }
+
+        private async Task MainAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await client.LoginAsync(TokenType.Bot, File.ReadAllText("Keys/token.txt"));
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("c.", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, msg);
+                await commands.ExecuteAsync(context, pos);
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            var cc = Console.ForegroundColor;
+            switch (msg.Severity)
+            {
+                case LogSeverity.Critical:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    break;
+                case LogSeverity.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case LogSeverity.Warning:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case LogSeverity.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+                case LogSeverity.Verbose:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case LogSeverity.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine(msg);
+            Console.ForegroundColor = cc;
+            return Task.CompletedTask;
+        }
+
+        private Task LogError(LogMessage msg)
+        {
+            CommandException ce = msg.Exception as CommandException;
+            if (ce != null)
+            {
+                ce.Context.Channel.SendMessageAsync("", false, new EmbedBuilder()
+                {
+                    Color = Color.Red,
+                    Title = msg.Exception.InnerException.GetType().ToString(),
+                    Description = "An error occured while executing last command.\nHere are some details about it: " + msg.Exception.InnerException.Message
+                }.Build());
+            }
+            return Task.CompletedTask;
+        }
+    }
+}

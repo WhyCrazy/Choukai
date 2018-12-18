df0d716fda9ed62dcc21f2bacc3fc3556d07e85a
Crispin Suminski <33662750+WhyCrazy@users.noreply.github.com>
2018-12-18 16:47:00 +0100

    Fixed english errors

--- Choukai/Modules/Communication.cs
+++ Choukai/Modules/Communication.cs
@@ -13,7 +13,7 @@ namespace Choukai.Modules
         public async Task Help(params string[] args)
         {
             await ReplyAsync("Member: Join Confiture as a member." + Environment.NewLine +
-                "BotInfo: Give informations about the bot");
+                "BotInfo: Gives information about the bot");
         }
 
         [Command("BotInfo"), Alias("InfoBot")]
@@ -26,18 +26,18 @@ namespace Choukai.Modules
         private async Task Member(params string[] args)
         {
             if (Context.Channel.Id != 514909469541531669) // Public General
-                await ReplyAsync("Ask in <#514909469541531669> for that kind of request.");
+                await ReplyAsync("Please request this in <#514909469541531669>.");
             else if (Context.Channel as ITextChannel == null)
-                await ReplyAsync("You can't ask this in private message.");
+                await ReplyAsync("You can't request this by private message.");
             else
             {
                 IGuildUser user = (IGuildUser)Context.User;
                 if (user.RoleIds.ToList().Contains(511569693807607830)) // Member
-                    await ReplyAsync("You already are a member.");
+                    await ReplyAsync("You are already a member.");
                 else
                 {
                     await user.AddRoleAsync(Context.Guild.GetRole(511569693807607830));
-                    await ReplyAsync("<@" + Context.User.Id +"> You are now a member, welcome in Confiture.");
+                    await ReplyAsync("<@" + Context.User.Id +"> You are now a member, welcome to Confiture.");
                 }
             }
         }
